d01b07aa12fb21462884e1b480c2bdb16f08a12b
shoq <shoq.dh@gmail.com>
2020-05-04 21:27:36 +0300

    last upadate

--- calculatorProj/ContentView.swift
+++ calculatorProj/ContentView.swift
@@ -9,8 +9,275 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Gcolor=Color.black
     var body: some View {
-        Text("Hello, World!")
+
+                ZStack{
+              Gcolor
+                 
+            VStack{
+            
+      Text("555")
+        .font(.system(size: 80))
+        .foregroundColor(.white)
+        .multilineTextAlignment(.trailing)
+        .frame(width: 300, height: 120, alignment: .bottomTrailing)
+                
+
+                
+                
+                
+                
+                
+                HStack {
+                    
+                   Text("C")
+                    .font(.system(size: 50))
+                   
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .bold()
+                    .frame(width: 80, height: 80, alignment:.center)
+                    .background(Color.gray)
+                     .clipShape(Capsule())
+                    .padding()
+                    .offset(x: 10, y: 0)
+                     Spacer()
+                    Text("±")
+                                     .font(.system(size: 50))
+                                    
+                                     .foregroundColor(.white)
+                                     .font(.title)
+                                     .bold()
+                                     .frame(width: 80, height: 80, alignment:.center)
+                                     .background(Color.gray)
+                                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    Spacer()
+                    Text("%")
+                     .font(.system(size: 50))
+                    
+                     .foregroundColor(.white)
+                     .font(.title)
+                     .bold()
+                     .frame(width: 80, height: 80, alignment:.center)
+                     .background(Color.gray)
+                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    Spacer()
+                                       Text("÷")
+                                        .font(.system(size: 50))
+                                       
+                                        .foregroundColor(.white)
+                                        .font(.title)
+                                        .bold()
+                                        .frame(width: 80, height: 80, alignment:.center)
+                                        .background(Color.orange)
+                                         .clipShape(Capsule())
+                                        .offset(x: -10, y: 0)
+                    .padding()
+                
+                    
+                }
+
+           HStack {
+               
+              Text("7")
+               .font(.system(size: 50))
+              
+               .foregroundColor(.white)
+               .font(.title)
+               .bold()
+               .frame(width: 80, height: 80, alignment:.center)
+               .background(Color.gray)
+                .clipShape(Capsule())
+               .padding()
+               .offset(x: 10, y: 0)
+           
+               Text("8")
+                                .font(.system(size: 50))
+                               
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .bold()
+                                .frame(width: 80, height: 80, alignment:.center)
+                                .background(Color.gray)
+                                 .clipShape(Capsule())
+               .offset(x: 5, y: 0)
+             
+               Text("9")
+                .font(.system(size: 50))
+               
+                .foregroundColor(.white)
+                .font(.title)
+                .bold()
+                .frame(width: 80, height: 80, alignment:.center)
+                .background(Color.gray)
+                 .clipShape(Capsule())
+               .offset(x: 5, y: 0)
+            
+                                  Text("x")
+                                   .font(.system(size: 50))
+                                  
+                                   .foregroundColor(.white)
+                                   .font(.title)
+                                   .bold()
+                                   .frame(width: 80, height: 80, alignment:.center)
+                                   .background(Color.orange)
+                                    .clipShape(Capsule())
+                                   .offset(x: -10, y: 0)
+               .padding()
+           
+               
+           }
+                
+           HStack {
+                    
+                   Text("4")
+                    .font(.system(size: 50))
+                   
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .bold()
+                    .frame(width: 80, height: 80, alignment:.center)
+                    .background(Color.gray)
+                     .clipShape(Capsule())
+                    .padding()
+                    .offset(x: 10, y: 0)
+                     
+                    Text("5")
+                                     .font(.system(size: 50))
+                                    
+                                     .foregroundColor(.white)
+                                     .font(.title)
+                                     .bold()
+                                     .frame(width: 80, height: 80, alignment:.center)
+                                     .background(Color.gray)
+                                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    
+                    Text("6")
+                     .font(.system(size: 50))
+                    
+                     .foregroundColor(.white)
+                     .font(.title)
+                     .bold()
+                     .frame(width: 80, height: 80, alignment:.center)
+                     .background(Color.gray)
+                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    
+                                       Text("-")
+                                        .font(.system(size: 50))
+                                       
+                                        .foregroundColor(.white)
+                                        .font(.title)
+                                        .bold()
+                                        .frame(width: 80, height: 80, alignment:.center)
+                                        .background(Color.orange)
+                                         .clipShape(Capsule())
+                                        .offset(x: -10, y: 0)
+                    .padding()
+                
+                    
+                }
+                HStack {
+                    
+                   Text("1")
+                    .font(.system(size: 50))
+                   
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .bold()
+                    .frame(width: 80, height: 80, alignment:.center)
+                    .background(Color.gray)
+                     .clipShape(Capsule())
+                    .padding()
+                    .offset(x: 10, y: 0)
+                     Spacer()
+                    Text("2")
+                                     .font(.system(size: 50))
+                                    
+                                     .foregroundColor(.white)
+                                     .font(.title)
+                                     .bold()
+                                     .frame(width: 80, height: 80, alignment:.center)
+                                     .background(Color.gray)
+                                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    Spacer()
+                    Text("3")
+                     .font(.system(size: 50))
+                    
+                     .foregroundColor(.white)
+                     .font(.title)
+                     .bold()
+                     .frame(width: 80, height: 80, alignment:.center)
+                     .background(Color.gray)
+                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    Spacer()
+                                       Text("+")
+                                        .font(.system(size: 50))
+                                       
+                                        .foregroundColor(.white)
+                                        .font(.title)
+                                        .bold()
+                                        .frame(width: 80, height: 80, alignment:.center)
+                                        .background(Color.orange)
+                                         .clipShape(Capsule())
+                                        .offset(x: -10, y: 0)
+                    .padding()
+                
+                    
+                }
+                
+                HStack {
+                    
+                   Text("0")
+                    .font(.system(size: 50))
+                   
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .bold()
+                    .frame(width: 160, height: 80, alignment:.center)
+                    .background(Color.gray)
+                     .clipShape(Capsule())
+                    .padding()
+                    .offset(x: 10, y: 0)
+                     Spacer()
+                    Text(".")
+                                     .font(.system(size: 50))
+                                    
+                                     .foregroundColor(.white)
+                                     .font(.title)
+                                     .bold()
+                                     .frame(width: 80, height: 80, alignment:.center)
+                                     .background(Color.gray)
+                                      .clipShape(Capsule())
+                    .offset(x: 5, y: 0)
+                    Spacer()
+                          Text("=")
+                                        .font(.system(size: 50))
+                                       
+                                        .foregroundColor(.white)
+                                        .font(.title)
+                                        .bold()
+                                        .frame(width: 80, height: 80, alignment:.center)
+                                        .background(Color.orange)
+                                         .clipShape(Capsule())
+                                        .offset(x: -10, y: 0)
+                    .padding()
+                
+                    
+                }
+                
+                
+                
+            
+        }
+        
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
